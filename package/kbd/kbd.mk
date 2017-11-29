fc798c28efbedfdb6716a363f7903af3deb05a33
Ingo Albrecht <prom@berlin.ccc.de>
2017-11-29 23:06:10 +0100

    kbd: made console fonts optional

--- package/kbd/kbd.mk
+++ package/kbd/kbd.mk
@@ -20,4 +20,20 @@ KBD_LICENSE_FILES = COPYING
 KBD_INSTALL_STAGING_OPTS = MKINSTALLDIRS=$(@D)/config/mkinstalldirs DESTDIR=$(STAGING_DIR) install
 KBD_INSTALL_TARGET_OPTS = MKINSTALLDIRS=$(@D)/config/mkinstalldirs DESTDIR=$(TARGET_DIR) install
 
+ifeq ($(BR2_PACKAGE_KBD_FONTS),y)
+# Clean installed fonts
+define KBD_CLEANUP_FONTS
+	# Remove README files
+	rm -f $(TARGET_DIR)/usr/share/consolefonts/README.*
+endef
+KBD_POST_INSTALL_TARGET_HOOKS += KBD_CLEANUP_FONTS
+else
+# Remove fonts
+define KBD_REMOVE_FONTS
+	rm -f $(TARGET_DIR)/usr/bin/setfont
+	rm -rf $(TARGET_DIR)/usr/share/consolefonts
+endef
+KBD_POST_INSTALL_TARGET_HOOKS += KBD_REMOVE_FONTS
+endif
+
 $(eval $(autotools-package))
